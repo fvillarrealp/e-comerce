6ed3fac4bb80d3ba4615d89997830298673a7e3f
Felipe Villarreal <fvillarrealp@unal.edu.co>
2020-10-31 18:30:20 -0500

    'firstMigrationsDone'

--- ecommerce/store/admin.py
+++ ecommerce/store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.Register(Customer)
+admin.site.Register(Product)
+admin.site.Register(Order)
+admin.site.Register(OrderItem)
+admin.site.Register(ShippingAddress)
 
 # Register your models here.
