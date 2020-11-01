b15a892b183a5131f1b94d83a27bf4713a27f0c1
Felipe Villarreal <fvillarrealp@unal.edu.co>
2020-11-01 03:34:16 -0500

    'ProductErrorSolved'

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Customer, Product, Order, OrderItem, ShippingAddress
 
 # Create your views here.
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products': products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
