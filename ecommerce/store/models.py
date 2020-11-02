d41f4184b411868aee50087c04de96112aa07b81
Felipe Villarreal <fvillarrealp@unal.edu.co>
2020-11-01 23:46:36 -0500

    'firstVersionCart'

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -36,6 +36,18 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.transaction_id)
+    
+    @property
+    def get_cart_total(self): # Show the total price to pay
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    @property
+    def get_cart_items(self): # Show the amount of items to pay 
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
 
 
 class OrderItem(models.Model):
@@ -43,6 +55,12 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True) # Select the current date
+
+    @property
+    def get_total(self): # Show the current price of the arrow
+        total = self.product.price * self.quantity
+        return total
+
 
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
