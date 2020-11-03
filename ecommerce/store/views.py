0646d398ddb7de39e38cc054303a1e2620c382f9
Felipe Villarreal <fvillarrealp@unal.edu.co>
2020-11-03 14:13:17 -0500

    'checkout2.0'

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -12,7 +12,7 @@ def cart(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer= customer, complete=False)
-        items = order.orderitem_set.all()
+        items = order.orderitem_set.all() # The reason the reverse is a queryset is, ForeignKey is 1-to-many relationship. Hence, the reverse is a queryset.
     else:
         items = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
@@ -21,5 +21,14 @@ def cart(request):
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all() # The reason the reverse is a queryset is, ForeignKey is 1-to-many relationship. Hence, the reverse is a queryset.
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
